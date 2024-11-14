44a843f1c759c5744af1d5d4fbe406302054bf80
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-14 16:08:56 +0100

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:mylocalization/mylocalization.dart';
 void main() {
   runApp(const MyApp());
 }
-
+void myfunction() {
+    print(myLocalization());
+  }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -58,17 +60,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -117,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: const FloatingActionButton(
-        onPressed: myLocalization,
+        onPressed: myfunction,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
--- lib/mylocalization.dart
+++ lib/mylocalization.dart
@@ -1,11 +1,9 @@
 library mylocalization;
+
 import 'dart:ui';
 
 String myLocalization() {
   var language = PlatformDispatcher.instance.locale.languageCode;
   //print(language);
-  return language;// int addOne(int value) => value + 1;
-}
-void main(){
-  print(myLocalization());
+  return language; // int addOne(int value) => value + 1;
 }
