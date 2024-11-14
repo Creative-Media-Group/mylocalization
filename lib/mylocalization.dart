8a202c6a52b7cd80686ff4a9c04793c6c72bb042
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-14 15:35:32 +0100

    Update mylocalization.dart

--- lib/mylocalization.dart
+++ lib/mylocalization.dart
@@ -1,8 +1,11 @@
 library mylocalization;
-import 'dart:io';
+import 'dart:ui';
 
 String myLocalization() {
-  final String language = Platform.localeName;
+  var language = PlatformDispatcher.instance.locale.languageCode;
   //print(language);
   return language;// int addOne(int value) => value + 1;
+}
+void main(){
+  print(myLocalization());
 }
