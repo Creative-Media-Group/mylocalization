46e0d02681da8e6f388e07309f426d7cf2c63be5
tct123 <42028373+tct123@users.noreply.github.com>
2024-11-14 16:12:33 +0100

    Update mylocalization.dart

--- lib/mylocalization.dart
+++ lib/mylocalization.dart
@@ -1,8 +1,10 @@
 library mylocalization;
 
 import 'dart:ui';
+import "package:flutter/widgets.dart";
 
 String myLocalization() {
+  WidgetsFlutterBinding.ensureInitialized();
   var language = PlatformDispatcher.instance.locale.languageCode;
   //print(language);
   return language; // int addOne(int value) => value + 1;
